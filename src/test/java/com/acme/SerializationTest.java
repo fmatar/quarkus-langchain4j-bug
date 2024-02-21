0c1b35443e4384231dc2c202cc34842d9d1952c3
Fady Matar <fady.matar@external.bcgdv.com>
2024-02-21 06:06:00 -0800

    Failure test update

--- src/test/java/com/acme/SerializationTest.java
+++ src/test/java/com/acme/SerializationTest.java
@@ -1,30 +1,33 @@
 package com.acme;
 
-import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.equalToObject;
-import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import dev.ai4j.openai4j.chat.ChatCompletionRequest;
-import dev.langchain4j.internal.Json;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 @QuarkusTest
 class SerializationTest {
-    @Test
-    void testHelloEndpoint() {
-       var request = ChatCompletionRequest.builder().model("gpt-3.5-turbo").build();
 
-       assertThat(request, notNullValue());
+  @Test
+  void testSerializationWithLangchain4jSerialization() {
+    var request = ChatCompletionRequest.builder().model("gpt-3.5-turbo").build();
+    assertThat(request, notNullValue());
+    var json = dev.langchain4j.internal.Json.toJson(request);
+    assertThat(json, notNullValue());
+    var deserialized = dev.langchain4j.internal.Json.fromJson(json, ChatCompletionRequest.class);
+    assertThat(deserialized, equalToObject(request));
+  }
 
-       var json = Json.toJson(request);
-       assertThat(json, notNullValue());
-
-       var deserialized = Json.fromJson(json, ChatCompletionRequest.class);
-
-       assertThat(deserialized, equalToObject(request));
-    }
-
+  @Test
+  void testSerializationWithQuarkusSerialization() {
+    var request = ChatCompletionRequest.builder().model("gpt-3.5-turbo").build();
+    assertThat(request, notNullValue());
+    var json = io.vertx.core.json.Json.encode(request);
+    assertThat(json, notNullValue());
+    var deserialized = io.vertx.core.json.Json.decodeValue(json, ChatCompletionRequest.class);
+    assertThat(deserialized, equalToObject(request));
+  }
 }
